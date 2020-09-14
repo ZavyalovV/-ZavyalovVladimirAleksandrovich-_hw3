a0ee8af4f91e1cc38740c86dcd79813a19e48fdc
zavyalov <zavyalovvla@yandex.ru>
2020-09-14 16:35:57 +0300

    final commit

--- file1.cpp
+++ file1.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
 using namespace std;
 int main() {
-<<<<<<< HEAD
     cout<<"Hello world"<<endl;
     cout<<"My new rating"<<endl;
-=======
     cout<<"Comment was added"<<endl;
->>>>>>> comment
 }
